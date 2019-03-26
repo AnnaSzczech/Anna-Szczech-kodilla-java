a6adcd17a845a54eb515ed60fd0ed79ac44a1c57
AnnaSzczech <anna.szczech89@gmail.com>
2019-03-26 19:31:39 +0100

    kodilla-good-patterns

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/GoodPattersMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/GoodPattersMain.java
@@ -2,12 +2,14 @@ package com.kodilla.good.patterns;
 
 import com.kodilla.good.patterns.challenges.MovieStore;
 
+import java.util.stream.Collectors;
+
 public class GoodPattersMain {
 
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
         String movieList = movieStore.getMovies().entrySet().stream()
-                .flatMap(movie -> movie.getValue().stream()).forEach(System.out::println);
+                .flatMap(movie -> movie.getValue().stream()).collect(Collectors.joining(" ! "));
         System.out.println(movieList);
     }
 }
