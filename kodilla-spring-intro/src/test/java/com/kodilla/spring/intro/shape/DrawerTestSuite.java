256a03f57e30c1300d093f739cca69b0fbc32f73
AnnaSzczech <anna.szczech89@gmail.com>
2019-04-02 21:52:52 +0200

    Modul 14.1

---
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.intro.shape;
+
+public class DrawerTestSuite {
+    @Test
+    public void testDoDrawing() {
+        //Given
+        Drawer drawer = new Drawer();
+        //When
+        drawer.doDrawing();
+        //Then
+        //do nothing
+    }
+}
