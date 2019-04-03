3a747a461c3a88171b3770550d103983f4e365fb
AnnaSzczech <anna.szczech89@gmail.com>
2019-04-03 20:12:46 +0200

    sproing

--- kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -1,13 +1,28 @@
 package com.kodilla.spring.intro.shape;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class DrawerTestSuite {
     @Test
-    public void testDoDrawing() {
+    public void testDoDrawingWithCircle() {
         //Given
-        Drawer drawer = new Drawer();
+        Circle circle = new Circle();
+        Drawer drawer = new Drawer(circle);
         //When
-        drawer.doDrawing();
+        String result = drawer.doDrawing();
         //Then
-        //do nothing
+        Assert.assertEquals("This is a circle", result);
+    }
+
+    @Test
+    public void testDoDrawingWithTriangle() {
+        //Given
+        Triangle triangle = new Triangle();
+        Drawer drawer = new Drawer(triangle);
+        //When
+        String result = drawer.doDrawing();
+        //Then
+        Assert.assertEquals("This is a triangle", result);
     }
 }
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.kodilla.spring.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRunnerTestSuite {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
