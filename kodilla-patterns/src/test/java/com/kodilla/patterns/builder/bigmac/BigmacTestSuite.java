219e390ab26f40563d807554e574e376186e05d3
AnnaSzczech <anna.szczech89@gmail.com>
2019-04-12 20:43:39 +0200

    Task 15.5

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -20,5 +20,26 @@ public class BigmacTestSuite {
         int howManyIngredients = bigmac.getIngredients().size();
         //then
         Assert.assertEquals(3, howManyIngredients);
+        Assert.assertEquals("z sezamem",  bigmac.getBun());
+        Assert.assertEquals("1000 wysp",  bigmac.getSauce());
+        Assert.assertEquals(2,  bigmac.getBurgers());
+    }
+
+    @Test
+    public void testBigMacNewWithoutSauce(){
+        //given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("bez sezamu")
+                .burgers(1)
+                .ingredients("ser")
+                .build();
+        System.out.println(bigmac);
+        //when
+        int howManyIngredients = bigmac.getIngredients().size();
+        //then
+        Assert.assertEquals(1, howManyIngredients);
+        Assert.assertEquals("bez sezamu",  bigmac.getBun());
+        Assert.assertEquals(1,  bigmac.getBurgers());
+        Assert.assertNull(bigmac.getSauce());
     }
 }
