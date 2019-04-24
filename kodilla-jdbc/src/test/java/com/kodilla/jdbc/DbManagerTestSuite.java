cbea1668106e031d69248980e5ac27b6d6b2b31a
AnnaSzczech <anna.szczech89@gmail.com>
2019-04-24 21:53:08 +0200

    Task 17.1

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-@Ignore
 public class DbManagerTestSuite {
     @Test
     public void testGetConnection() throws SQLException {
